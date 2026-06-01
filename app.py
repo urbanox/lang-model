7b59ba600a6e7aa7943c8d5e0c7b826f1f05b77b
Pavel Urban <P.Urban@consultant.crif.com>
2026-06-01 11:56:39 +0200

    Generating names first

--- app.py
+++ app.py
@@ -18,10 +18,12 @@ alfabet.sort()
 def stoi(c):
     return alfabet.index(c)
 
+def itos(index):
+    return alfabet[index]
+
 alfabet_len = len(alfabet)
 
-model = torch.zeros(alfabet_len, alfabet_len, dtype=torch.int)
-print(model[0][0])
+model = torch.zeros(alfabet_len, alfabet_len, dtype=torch.float)
 
 for name in names:
     for i, c in enumerate(name[:-1]):
@@ -29,19 +31,52 @@ for name in names:
         y = stoi(name[i+1])
         model[x, y] += 1
 
-print(model[5, 5].data)
 
-plt.imshow(model.cpu().numpy().T)
+def print_image():
+    plt.imshow(model.cpu().numpy().T)
 
-for i in range(alfabet_len):
-    for j in range(alfabet_len):
-        plt.text(i, j, model[i, j].item(), fontsize=6, ha='center', va='center')
+    for i in range(alfabet_len):
+        for j in range(alfabet_len):
+            plt.text(i, j, f"{model[i, j].item():.2f}", fontsize=6, ha='center', va='center')
+            
+    plt.xticks(range(alfabet_len), alfabet)
+    plt.yticks(range(alfabet_len), alfabet)
+    ax = plt.gca()
+    ax.xaxis.tick_top()
+    plt.colorbar()
+    plt.title("SK mena")
+    plt.show()
+
+
+
+for x_index in range(alfabet_len):
+    sum = 0
+    for y_index in range(alfabet_len):
+        sum += model[x_index, y_index].item()
+
+    for y_index in range(alfabet_len):
+        model[x_index, y_index] = model[x_index, y_index] / sum
+
+current_index = 0
+for i in range(10):
+    name = ""
+    while True:
+        if not torch.isclose(model[current_index, :].sum(), torch.tensor(1.0)):
+            raise "Wrong probability"
         
-plt.xticks(range(alfabet_len), alfabet)
-plt.yticks(range(alfabet_len), alfabet)
-ax = plt.gca()
-ax.xaxis.tick_top()
-plt.colorbar()
-plt.title("SK mena")
-plt.show()
+        next_index = torch.multinomial(model[current_index, :], num_samples=1).item()
+        print(f"probability: {model[current_index, :][next_index]}")
+        # print(f"Index: {next_index}, '{itos(next_index)}'")
+        
+        next_char = itos(next_index)
+        if next_char == '.':
+            print(name) 
+            break
 
+        name += next_char
+        current_index = next_index
+
+# print(model.sum(dim=1, keepdim=True))
+
+# print(model[0])
+# print_image()
